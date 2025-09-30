3cd79575e90a6b8585886bf9c3b78607a91386a1
Zulfiquar Shaikh <83855661+shaikh111-Z@users.noreply.github.com>
2025-09-30 23:15:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 # Query fruit options
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width = True)
 st.stop()                                                                      
 # Convert Snowpark DataFrame to list of fruit names
